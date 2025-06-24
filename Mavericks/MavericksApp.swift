6fbc569359e40bf696ac9af34ddeaa05e3f7615b
MksMil <maksymmiliaiev@gmail.com>
2025-06-24 15:24:23 +0300

    Initial Commit

---
+++ Mavericks/MavericksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MavericksApp.swift
+//  Mavericks
+//
+//  Created by Миляев Максим on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MavericksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
